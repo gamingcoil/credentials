b9ae47f0aea7884771438fdab51f618707c489e6
gamingcoil <84976465+gamingcoil@users.noreply.github.com>
2023-07-12 09:18:55 +0300

    Update queryofid.js

--- queryofid.js
+++ queryofid.js
@@ -1,30 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const itemId = urlParams.get('id');
-  
-    fetch(`https://credentials-server.onrender.com/items/${itemId}`)
-      .then(response => response.json())
-      .then(item => {
-        const itemListElement = document.getElementById('item-list');
-        const itemElement = document.createElement('div');
-        itemElement.classList.add('item-box');
-  
-        const imageElement = document.createElement('img');
-        imageElement.src = item.imageLink;
-        itemElement.appendChild(imageElement);
-  
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = item.title;
-        itemElement.appendChild(titleElement);
-  
-        const descriptionElement = document.createElement('p');
-        descriptionElement.textContent = item.description;
-        itemElement.appendChild(descriptionElement);
-  
-        itemListElement.appendChild(itemElement);
-      })
-      .catch(error => {
-        console.error('Failed to fetch item:', error);
-      });
-  });
-  
+  const urlParams = new URLSearchParams(window.location.search);
+  const itemId = urlParams.get('id');
+
+  fetch(`https://credentials-server.onrender.com/items/${itemId}`)
+    .then(response => response.json())
+    .then(item => {
+      const itemListElement = document.getElementById('item-list');
+      const itemElement = document.createElement('div');
+      itemElement.classList.add('item-box');
+
+      const imageElement = document.createElement('img');
+      imageElement.src = item.imageLink;
+      itemElement.appendChild(imageElement);
+
+      const itemDetailsElement = document.createElement('div');
+      itemDetailsElement.classList.add('item-details');
+
+      const titleElement = document.createElement('h2');
+      titleElement.textContent = item.title;
+      itemDetailsElement.appendChild(titleElement);
+
+      const descriptionElement = document.createElement('p');
+      descriptionElement.textContent = item.description;
+      itemDetailsElement.appendChild(descriptionElement);
+
+      itemElement.appendChild(itemDetailsElement);
+
+      itemListElement.appendChild(itemElement);
+    })
+    .catch(error => {
+      console.error('Failed to fetch item:', error);
+    });
+});
