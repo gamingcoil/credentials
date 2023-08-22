b04d75e2c46f7ab16926f6cbc42f2362c0c1bf05
gamingcoil <84976465+gamingcoil@users.noreply.github.com>
2023-08-22 20:29:47 +0300

    Update queryofid.js

--- queryofid.js
+++ queryofid.js
@@ -20,6 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
       const descriptionElement = document.createElement('p');
       descriptionElement.textContent = item.description;
+      const urlBtn = document.createElement('button');
+      urlBtn.type = "submit";
+      urlBtn.textContent = 'To Website'
+      urlBtn.formAction = item.websiteUrl;
+      descriptionElement.appendChild(urlBtn)
       itemDetailsElement.appendChild(descriptionElement);
 
       serviceContent.appendChild(itemDetailsElement);
